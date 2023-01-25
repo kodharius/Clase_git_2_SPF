07b77076f22f851118c169dc0f56420767249231
Dario Castro <if719910@iteso.mx>
2023-01-24 21:18:24 -0600

    cambio est 19

---
+++ Ejercicio_clase2_git.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Ejercicios clase 2 git
+# 
+# Este cuaderno de python es creado con el fin de ilustrar el método de fork y pull request. 
+# 
+# Este es el cambio desde local desde el admon.
+
+# # 1
+# La sucesión de Fibonacci se define de la siguiente forma: a1=1, a2=1 y an=an-1+an-2 para n>2, es decir los dos primeros son 1 y el resto cada uno es la suma de los dos anteriores, los primeros son: 1, 1, 2, 3, 5, 8, 13, 21, ... Hacer un diagrama de flujo para calcular el Nésimo término de la sucesión.
+# 
+# # 2 
+# Elabora un programa que solicite la medida en Pies y realice la conversión a pulgadas, yardas, cm y metros. Toma en cuenta que un pie tiene 12 pulgadas y una pulgada equivale a 2.54 cm.
+# 
+# # 3
+# Escribir un programa que pregunte al usuario su nombre, edad, dirección y teléfono y lo guarde en un diccionario. Después debe mostrar por pantalla el mensaje <nombre> tiene <edad> años, vive en <dirección> y su número de teléfono es <teléfono>.
+#     
+# # 4
+# Escribir un programa que cree un diccionario vacío y lo vaya llenado con información sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) que se le pida al usuario. Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.
+#     
+# # 5
+# Escribir una función que tenga la capacidad de convertir unidades de longitud (al menos 5 distintas unidades), de temperatura (celcius, fahrenheit, kelvin) y masa (al menos 3 distintas unidades). La función debe de recibir tres argumentos. El primero será un string especificando si se desea hacer una conversión de longitud, temperatura o masa. El segundo será otro string que especica cuál será la unidad que se desea convertir (ejemplo centigrados, kilogramos o kilómetros. Finalmente el tercer argumento será un string que especifica, cuál será la unidad de destino a la que se tiene que convertir. Se debe de implemetar la lógica adecuada para que si se está en la unidad de conversión longitud el tercer argumento esté acorde a las posibilidades de longitud y no de temperatura o masa.
+
+# In[1]:
+
+
+# Profesor
+import numpy as np
+ejercicios = np.tile(np.arange(1,6), 6)
+estudiantes = np.arange(1, 31)
+clase = []
+
+for est, ejer in zip(estudiantes, ejercicios):
+    clase.append(f'estudiante {est} ejercicio {ejer}')
+    
+clase
+
+
+# In[1]:
+
+
+# 1 - ejercicio 1
+# Nombre 
+
+
+# In[ ]:
+
+
+# 2 - ejercicio 2
+# Nombre
+
+
+# In[ ]:
+
+
+# 3 - ejercicio 3
+# Nombre 
+
+
+# In[ ]:
+
+
+# 4 - ejercicio 4
+# Nombre
+
+
+# In[4]:
+
+
+# 5 - ejercicio 5
+# Nombre
+
+
+# In[ ]:
+
+
+# 6 - ejercicio 1
+# Nombre
+
+
+# In[ ]:
+
+
+# 7 - ejercicio 2
+# Nombre
+
+
+# In[ ]:
+
+
+# 8 - ejercicio 3
+# Nombre
+
+
+# In[ ]:
+
+
+# 9 - ejercicio 4
+# Nombre
+
+
+# In[ ]:
+
+
+# 10 - ejercicio 5
+# Nombre
+
+
+# In[5]:
+
+
+# 11 - ejercicio 1
+# Nombre
+
+
+# In[ ]:
+
+
+# 12 - ejercicio 2
+# Nombre
+
+
+# In[6]:
+
+
+# 13 - ejercicio 3
+# Nombre
+
+
+# In[ ]:
+
+
+# 14 - ejercicio 4
+# Nombre
+
+
+# In[ ]:
+
+
+# 15 - ejercicio 5
+# Nombre 
+
+
+# In[ ]:
+
+
+# 16 - ejercicio 1
+# Nombre
+
+
+# In[ ]:
+
+
+# 17 - ejercicio 2
+# Nombre
+
+
+# In[ ]:
+
+
+# 18  - ejercicio 3
+# Nombre
+
+
+# In[ ]:
+
+
+# 19 - ejercicio 4
+# Nombre Dario Castro G
+
+
+# In[ ]:
+
+
+# 20 - ejercicio 5
+# Nombre
+
+
+# In[7]:
+
+
+# 21 - ejercicio 1
+# Nombre
+
+
+# In[ ]:
+
+
+# 22 - ejercicio 2
+#Nombre
+
+
+# In[ ]:
+
+
+# 23 - ejercicio 3
+# Nombre
+
+
+# In[ ]:
+
+
+# 24 - ejercicio 4
+# Nombre
+
+
+# In[ ]:
+
+
+# 25 - ejercicio 5
+# Nombre
+
+
+# In[ ]:
+
+
+# 26 - ejercicio 1
+# Nombre
+
+
+# In[ ]:
+
+
+# 27 - ejercicio 2
+# Nombre
+
+
+# In[ ]:
+
+
+# 28 - ejercicio 3
+# Nombre
+
+
+# In[ ]:
+
+
+# 29 - ejercicio 4
+# Nombre
+
+
+# In[ ]:
+
+
+# 30 - ejercicio 5
+# Nombre
+
